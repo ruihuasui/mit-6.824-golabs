7afece34f7e7901949dfc5b24af58abe6ec2e980
Shawn Sui <ruihua.sui@gmail.com>
2022-06-30 02:04:24 +0800

    add go examples

--- golang/crawler.go
+++ golang/crawler.go
@@ -1,12 +1,3 @@
-// 
-// From Golang Tutorial
-// Exercise: Web Crawler - https://go.dev/tour/concurrency/10
-//
-// In this exercise you'll use Go's concurrency features to parallelize a web crawler.
-// Modify the Crawl function to fetch URLs in parallel without fetching the same URL twice.
-// Hint: you can keep a cache of the URLs that have been fetched on a map, but maps alone are not safe for concurrent use!
-// 
-
 package main
 
 import (
@@ -14,50 +5,137 @@ import (
 	"sync"
 )
 
-type Fetcher interface {
-	// Fetch returns the body of URL and
-	// a slice of URLs found on that page.
-	Fetch(url string) (body string, urls []string, err error)
-}
+//
+// Several solutions to the crawler exercise from the Go tutorial
+// https://tour.golang.org/concurrency/10
+//
 
-func quitCrawl(quit chan bool) {
-	quit <- true
-}
+//
+// Serial crawler
+//
 
-// Crawl uses fetcher to recursively crawl
-// pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher, quit chan bool) {
-	// TODO: Fetch URLs in parallel.
-	// TODO: Don't fetch the same URL twice.
-	// This implementation doesn't do either:
-	defer quitCrawl(quit)
-	urlVisited := urlCache.Reserve(url)
-	fmt.Println(url, urlVisited)
-	if depth <= 0 || urlVisited {
+func Serial(url string, fetcher Fetcher, fetched map[string]bool) {
+	if fetched[url] {
 		return
 	}
-	body, urls, err := fetcher.Fetch(url)
+	fetched[url] = true
+	urls, err := fetcher.Fetch(url)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
-	fmt.Printf("found: %s %q\n", url, body)
-	urlCache.SetBody(url, body)
 	for _, u := range urls {
-		quitChild := make(chan bool)
-		go Crawl(u, depth-1, fetcher, quitChild)
-		<-quitChild
+		Serial(u, fetcher, fetched)
 	}
-
 	return
 }
 
-func main() {
-	quit := make(chan bool)
-	go Crawl("https://golang.org/", 4, fetcher, quit)
-	<-quit
+//
+// Concurrent crawler with shared state and Mutex
+//
 
-	fmt.Println(urlCache.results)
+type fetchState struct {
+	mu      sync.Mutex
+	fetched map[string]bool
+}
+
+func ConcurrentMutex(url string, fetcher Fetcher, f *fetchState) {
+	f.mu.Lock()
+	already := f.fetched[url]
+	f.fetched[url] = true
+	f.mu.Unlock()
+
+	if already {
+		return
+	}
+
+	urls, err := fetcher.Fetch(url)
+	if err != nil {
+		return
+	}
+	var done sync.WaitGroup
+	for _, u := range urls {
+		done.Add(1)
+        u2 := u
+		go func() {
+			defer done.Done()
+			ConcurrentMutex(u2, fetcher, f)
+		}()
+		//go func(u string) {
+		//	defer done.Done()
+		//	ConcurrentMutex(u, fetcher, f)
+		//}(u)
+	}
+	done.Wait()
+	return
+}
+
+func makeState() *fetchState {
+	f := &fetchState{}
+	f.fetched = make(map[string]bool)
+	return f
+}
+
+//
+// Concurrent crawler with channels
+//
+
+func worker(url string, ch chan []string, fetcher Fetcher) {
+	urls, err := fetcher.Fetch(url)
+	if err != nil {
+		ch <- []string{}
+	} else {
+		ch <- urls
+	}
+}
+
+func master(ch chan []string, fetcher Fetcher) {
+	n := 1
+	fetched := make(map[string]bool)
+	for urls := range ch {
+		for _, u := range urls {
+			if fetched[u] == false {
+				fetched[u] = true
+				n += 1
+				go worker(u, ch, fetcher)
+			}
+		}
+		n -= 1
+		if n == 0 {
+			break
+		}
+	}
+}
+
+func ConcurrentChannel(url string, fetcher Fetcher) {
+	ch := make(chan []string)
+	go func() {
+		ch <- []string{url}
+	}()
+	master(ch, fetcher)
+}
+
+//
+// main
+//
+
+func main() {
+	fmt.Printf("=== Serial===\n")
+	Serial("http://golang.org/", fetcher, make(map[string]bool))
+
+	fmt.Printf("=== ConcurrentMutex ===\n")
+	ConcurrentMutex("http://golang.org/", fetcher, makeState())
+
+	fmt.Printf("=== ConcurrentChannel ===\n")
+	ConcurrentChannel("http://golang.org/", fetcher)
+}
+
+//
+// Fetcher
+//
+
+type Fetcher interface {
+	// Fetch returns a slice of URLs found on the page.
+	Fetch(url string) (urls []string, err error)
 }
 
 // fakeFetcher is Fetcher that returns canned results.
@@ -68,69 +146,45 @@ type fakeResult struct {
 	urls []string
 }
 
-func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+func (f fakeFetcher) Fetch(url string) ([]string, error) {
 	if res, ok := f[url]; ok {
-		return res.body, res.urls, nil
+		fmt.Printf("found:   %s\n", url)
+		return res.urls, nil
 	}
-	return "", nil, fmt.Errorf("not found: %s", url)
+	fmt.Printf("missing: %s\n", url)
+	return nil, fmt.Errorf("not found: %s", url)
 }
 
 // fetcher is a populated fakeFetcher.
 var fetcher = fakeFetcher{
-	"https://golang.org/": &fakeResult{
+	"http://golang.org/": &fakeResult{
 		"The Go Programming Language",
 		[]string{
-			"https://golang.org/pkg/",
-			"https://golang.org/cmd/",
+			"http://golang.org/pkg/",
+			"http://golang.org/cmd/",
 		},
 	},
-	"https://golang.org/pkg/": &fakeResult{
+	"http://golang.org/pkg/": &fakeResult{
 		"Packages",
 		[]string{
-			"https://golang.org/",
-			"https://golang.org/cmd/",
-			"https://golang.org/pkg/fmt/",
-			"https://golang.org/pkg/os/",
+			"http://golang.org/",
+			"http://golang.org/cmd/",
+			"http://golang.org/pkg/fmt/",
+			"http://golang.org/pkg/os/",
 		},
 	},
-	"https://golang.org/pkg/fmt/": &fakeResult{
+	"http://golang.org/pkg/fmt/": &fakeResult{
 		"Package fmt",
 		[]string{
-			"https://golang.org/",
-			"https://golang.org/pkg/",
+			"http://golang.org/",
+			"http://golang.org/pkg/",
 		},
 	},
-	"https://golang.org/pkg/os/": &fakeResult{
+	"http://golang.org/pkg/os/": &fakeResult{
 		"Package os",
 		[]string{
-			"https://golang.org/",
-			"https://golang.org/pkg/",
+			"http://golang.org/",
+			"http://golang.org/pkg/",
 		},
 	},
 }
-
-// SafeCounter is safe to use concurrently.
-type SafeURLCache struct {
-	mu sync.Mutex
-	results  map[string]string
-}
-
-// Value returns the current value of the counter for the given key.
-func (uc *SafeURLCache) Reserve(url string) bool {
-	uc.mu.Lock()
-	defer uc.mu.Unlock()
-	_, visited := uc.results[url]
-	if !visited {
-		uc.results[url] = "<EMPTY BODY>"
-	}
-	return visited
-}
-
-// Value returns the current value of the counter for the given key.
-func (uc *SafeURLCache) SetBody(url, body string) {
-	uc.mu.Lock()
-	defer uc.mu.Unlock()
-	uc.results[url] = body
-}
-
-var urlCache = &SafeURLCache{results:make(map[string]string)}
